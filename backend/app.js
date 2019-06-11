ccc9721d22a2eb589add79b5683f544ecc3df9f9
abhishek-shrm <abhishek.shrm.2195@gmail.com>
2019-06-11 20:38:09 +0530

    some more additions

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,21 @@
 var express=require('express');
 var path=require('path');
+var bodyParser= require('body-parser');
+var cors=require('cors');
 
 //Init app
 var app=express();
 
-console.log('hello');
+app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+  res.send('server is running');
+});
+
+//Start the server
+var port=3000;
+app.listen(process.env.PORT||port,()=>{
+  console.log('Server started on port '+port)
+});
